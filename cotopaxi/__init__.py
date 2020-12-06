97b4be4f6582611143a4ed7cd304202129c46ed9
Jakub Botwicz <@gmail.com>
2020-12-06 13:35:30 +0100

    Added logging setting for TensorFlow

--- cotopaxi/__init__.py
+++ cotopaxi/__init__.py
@@ -21,5 +21,27 @@
 #
 
 import logging
+import os
+
+# Logging settings for Scapy
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+# Logging settings for TensorFlow
+
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+
+#    TF_CPP_MIN_LOG_LEVEL - which has 3 or 4 basic levels
+#                          -> low numbers = more messages.
+#        0 outputs Information, Warning, Error, and Fatals (default)
+#        1 outputs Warning, and above
+#        2 outputs Errors and above.
+#    TF_CPP_MIN_VLOG_LEVEL - which causes very very many extra Information errors
+#                          - really for debugging only -> low numbers = less messages.
+#        3 Outputs lots and lots of stuff
+#        2 Outputs less
+#        1 Outputs even less
+#        0 Outputs nothing extra (default)
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+os.environ["TF_CPP_MIN_VLOG_LEVEL"] = "0"
